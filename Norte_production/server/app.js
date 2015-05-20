1de758c1c1ef2a981a7f196ed44d7eee8841e70b
James Leone <jamesleone@wilfadm63.mc.yu.edu>
2015-05-20 16:56:00 -0400

    adding files

---
+++ Norte_production/server/app.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('static-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+// var bodyParser = require('body-parser');
+// var db = require('./database');
+
+var app = express();
+
+app.use(favicon());
+app.use(logger('dev'));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded());
+// app.use(cookieParser());
+
+// bodyParser = require("body-parser")
+// app.use(bodyParser.json())
+//    .use(bodyParser.urlencoded())
+
+
+/**
+ * Development Settings
+ */
+if (app.get('env') === 'development') {
+
+	// This will change in production since we'll be using the dist folder
+	// This covers serving up the index page
+	app.use(express.static(path.join(__dirname, '../client/.tmp')));
+	app.use(express.static(path.join(__dirname, '../client/app')));
+
+}
+
+/**
+ * Production Settings
+ */
+if (app.get('env') === 'production') {
+
+	// changes it to use the optimized version for production
+	app.use(express.static(path.join(__dirname, '/dist')));
+
+}
+
+/**
+ * Routes
+ */
+// var router = require('./router')(app);
+
+// Error Handling
+app.use(function(err, req, res, next) {
+	res.status(err.status || 500);
+});
+
+module.exports = app;
