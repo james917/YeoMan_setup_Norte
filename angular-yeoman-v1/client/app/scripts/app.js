2ddd1d1bca8d4f71a28d57c2b46cbc92c1d3776a
James Leone <jamesleone@wilfadm63.mc.yu.edu>
2015-05-20 16:53:13 -0400

    adding files

---
+++ angular-yeoman-v1/client/app/scripts/app.js
@@ -0,0 +1,23 @@
+'use strict';
+
+angular
+  .module('clientApp', [
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/signup', {
+        templateUrl: 'views/signup.html',
+        controller: 'SignupCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
